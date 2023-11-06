1edd37ed2177f7f70dc30d600a1fd66c15b64c94
Adarsh4775 <137145969+Adarsh4775@users.noreply.github.com>
2023-11-06 20:19:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
 document.addEventListener('DOMContentLoaded', function () {
     const codeInputs = document.querySelectorAll('.code');
+    let focusedInputIndex = 0;
+
+    codeInputs[focusedInputIndex].focus();
 
     codeInputs.forEach((input, index) => {
         input.addEventListener('input', (e) => {
             const value = e.target.value;
 
-            if (value.length === 1 && index < codeInputs.length - 1) {
-                codeInputs[index + 1].focus();
+            if (value.length === 1 && focusedInputIndex < codeInputs.length - 1) {
+                focusedInputIndex++;
+                codeInputs[focusedInputIndex].focus();
             }
         });
 
         input.addEventListener('keydown', (e) => {
-            if (e.key === 'Backspace' && index > 0) {
-                e.preventDefault();
-                input.value = '';
-                codeInputs[index - 1].focus();
+            if (e.key === 'Backspace') {
+                if (focusedInputIndex > 0) {
+                    e.preventDefault();
+                    input.value = '';
+                    focusedInputIndex--;
+                    codeInputs[focusedInputIndex].focus();
+                }
             }
         });
     });
